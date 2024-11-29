e4fdf17aae416932dca97d89f62ae4e00612399f
Yevhen Yarynich <evgenyarunich@gmail.com>
2024-11-29 20:11:33 +0200

    moved to new version of translation

---
+++ js/translate.js
@@ -0,0 +1,103 @@
+function loadTranslations() {
+    const url = window.location.href;
+
+    var params = new URLSearchParams(window.location.search);
+
+
+    var location = params.get('location');
+    if(!location || location != 'it'){
+        location = 'ua';
+    }
+
+    fetch('/trans/'+location+'.json')
+        .then(response => {
+            if (!response.ok) {
+                throw new Error('file not found');
+            }
+            return response.json();
+        })
+        .then(data => {
+            applyTranslations(data); 
+   
+        })
+        .catch(error => {
+            console.error('Error:', error);
+        });
+
+
+
+    //change links 
+
+    if (params.toString()) {
+        document.querySelectorAll('a:not(.external)').forEach(link => {
+            const currentHref = link.href;
+            const separator = currentHref.includes('?') ? '&' : '?';
+            link.href = currentHref + separator + params.toString();
+        });
+    }
+
+    //add class active in change local button
+
+    const element = document.querySelector('.translate-switch a[attr-trans="'+location+'"]');
+
+    if (element) {
+        element.classList.add('active');
+    }
+
+    //check device first visit local
+
+    if (!localStorage.getItem('local')) {
+
+        const userLocale = navigator.language || navigator.userLanguage;
+
+        if (userLocale.startsWith('it') &&  location == 'ua') {
+             localStorage.setItem('local', 1);
+            const currentUrl = window.location.href;
+            const url = new URL(currentUrl);
+
+            url.searchParams.set('location', 'it'); 
+
+            window.location.href = url.toString();
+        }else{
+             localStorage.setItem('local', 1);
+        }
+    }
+
+
+}
+
+
+
+function applyTranslations(translations) {
+
+    const elements = document.querySelectorAll('[data-i18n]');
+    elements.forEach(el => {
+        const key = el.getAttribute('data-i18n');
+        if (translations[key]) {
+            el.innerHTML = translations[key];
+        } else {
+            console.warn(`Key '${key}' not found in json trans`);
+        }
+    });
+}
+
+
+
+
+document.addEventListener('DOMContentLoaded', () => {
+    document.querySelectorAll('.translate-switch a').forEach(element => {
+        element.addEventListener('click', function (event) {
+        
+            event.preventDefault();
+            const attrTrans = this.getAttribute('attr-trans');
+            const href = this.getAttribute('href');
+
+            if(href && href != '#'){
+                localStorage.setItem('local', attrTrans);
+                 window.location.href = href;
+            }
+        });
+    });
+});
+
+
